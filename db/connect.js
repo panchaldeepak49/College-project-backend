2dcf81e2a3c7e260d1cdb2aa7b7f05db2dcfb22b
Deepak <panchaldeepak49@gmail.com>
2024-01-14 19:07:56 +0530

    Mongodb atlas added

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,8 @@
 const mongoose = require ('mongoose');
 
-uri = 'mongodb://0.0.0.0:27017/wscube';          //database name provided(wscube)
+//uri = 'mongodb://0.0.0.0:27017/wscube';          //database name provided(wscube)
+uri = 'mongodb+srv://panchaldeepak49:Deepak%4012345@cluster1.ebo66nu.mongodb.net/' 
+                           //database name provided(cluster1)---Mongodb atlas cloud service used here
 
 const connectDB = ()=>{
     return mongoose.connect(uri,{
